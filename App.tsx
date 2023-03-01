12114c17ce165906faa30a80e5eb59595994e1dd
samirjabib <samirdev9669@gmail.com>
2023-03-01 13:36:37 -0500

    add tab botton

--- App.tsx
+++ App.tsx
@@ -5,17 +5,20 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {store} from './src';
 import {Provider} from 'react-redux';
 import {StackNavigator} from './src/navigator/StackNavigator';
-import { persistor } from './src/store/store';
-import { BottonTab } from './src/navigator/Tabs';
+import {persistor} from './src/store/store';
+import {BottonTab} from './src/navigator/Tabs';
+import { View, StyleSheet } from 'react-native';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <NavigationContainer>
-        {/* <StackNavigator /> */}
-        <BottonTab/>
-      </NavigationContainer>
+        <View style={{flex:1, backgroundColor:'white'}}>
+          <NavigationContainer>
+            {/* <StackNavigator /> */}
+            <BottonTab />
+          </NavigationContainer>
+        </View>
       </PersistGate>
     </Provider>
   );
--- src/navigator/Tabs.tsx
+++ src/navigator/Tabs.tsx
@@ -11,9 +11,20 @@ const Tab = createBottomTabNavigator();
 export const BottonTab = ()=> {
   return (
     <Tab.Navigator
+    
+    screenOptions={({ route }) => ({
+      tabBarStyle:{
+        backgroundColor:'#3dc7ec',
+        borderTopLeftRadius:17,
+        borderTopRightRadius:17,
+        height:60,
+
+        backfaceVisibility:'hidden'
+      },
+      })}
     >
-      <Tab.Screen name="Home" options={{tabBarIcon:() => <AntDesing name='home' color={'#3dc7ec'} size={30}/>}} component={Home} />
-      <Tab.Screen name="Menu" options={{tabBarIcon:() => <AntDesing name='menu-fold' color={'#3dc7ec'} size={30}/>}} component={Login}/>
+      <Tab.Screen name="Home"   options={{tabBarIcon:() => <AntDesing name='home' color={'white'} size={30}/>}} component={Home} />
+      <Tab.Screen name="Login" options={{tabBarIcon:() => <AntDesing name='menu-fold' color={'white'} size={30}/>}} component={Login}/>
     </Tab.Navigator>
   );
 }
