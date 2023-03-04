36cd18dbbf99d18c50f374473555b5d961e866a5
samirjabib <samirdev9669@gmail.com>
2023-03-04 05:26:14 -0500

    add maps

---
+++ src/components/Loading.tsx
@@ -0,0 +1,16 @@
+import { ActivityIndicator, View } from 'react-native'
+
+export const Loading = () => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <ActivityIndicator 
+                size={ 50 }
+                color="black"
+            />
+        </View>
+    )
+}
--- src/features/maps/components/Map/Map.tsx
+++ src/features/maps/components/Map/Map.tsx
@@ -1,17 +1,78 @@
-import MapView from 'react-native-maps';
+import MapView, {Marker} from 'react-native-maps';
+import {useEffect, useState, useRef} from 'react';
+import {useLocation} from '../../../../hook/useLocation';
+import { Loading } from '../../../../components/Loading';
+
+
+
 
 export const Map = () => {
+
+
+  const [showPolyline, setShowPolyline] = useState(true);
+
+  const {
+    hasLocation,
+    initialPosition,
+    getCurrentLocation,
+    followUserLocation,
+    userLocation,
+    stopFollowUserLocation,
+    routeLines,
+  } = useLocation();
+
+  const mapViewRef = useRef<MapView>();
+  const following = useRef<boolean>(true);
+
+  useEffect(() => {
+    followUserLocation();
+    return () => {
+      stopFollowUserLocation();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!following.current) return;
+
+    const {latitude, longitude} = userLocation;
+    mapViewRef.current?.animateCamera({
+      center: {latitude, longitude},
+    });
+  }, [userLocation]);
+
+  const centerPosition = async () => {
+    const {latitude, longitude} = await getCurrentLocation();
+
+    following.current = true;
+
+    mapViewRef.current?.animateCamera({
+      center: {latitude, longitude},
+    });
+  };
+
+  if (!hasLocation) {
+    return <Loading />;
+  }
+
   return (
     <>
       <MapView
         style={{flex: 1}}
+        showsUserLocation
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialPosition.latitude,
+          longitude: initialPosition.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+        onTouchStart={ () => following.current = false}
+      >
+        <Marker 
+          draggable
+          coordinate={initialPosition}
+          image={require('../../../../assets/car.png')}
+        />
+      </MapView>
     </>
   );
 };
